56b8b05592435340013f09651bb726d14fce0182
SamPrisG223 <34067118+SamPrisG223@users.noreply.github.com>
2021-08-26 08:58:15 -0700

    MenuNav@mediaupdate

--- Nav.js
+++ Nav.js
@@ -2,10 +2,10 @@ const toggleMenu = document.getElementsByClassName('toggle')[0]
 const menuUl = document.getElementsByClassName('menubarul')[0]
 
 toggleMenu.addEventListener('click', () => {
-   if (menuUl.style.display ==='none'){
-       menuUl.style.display='flex'       
+   if (menuUl.style.display ==='flex'){
+       menuUl.style.display='none'       
    }
    else{
-    menuUl.style.display='none' 
+    menuUl.style.display='flex' 
    }
 })
