204fdec8a1741141aefa67c26db066f8090e04ce
SamPrisG223 <34067118+SamPrisG223@users.noreply.github.com>
2021-08-27 12:34:10 -0700

    Submenu list well stacked

--- Nav.js
+++ Nav.js
@@ -1,9 +1,11 @@
 const toggleMenu = document.getElementsByClassName('toggle')[0]
 const menuUl = document.getElementsByClassName('menubarul')[0]
+const menu = document.getElementsByClassName('body')
 
 toggleMenu.addEventListener('click', () => {
    if (menuUl.style.display ==='flex'){
-       menuUl.style.display='none'       
+       menuUl.style.display='none'   
+           
    }
    else{
     menuUl.style.display='flex' 
